f418df107b76dd15eb5ba79f41eb857e6088c50c
18407778058 <1520064968@qq.com>
2025-04-19 22:03:02 +0800

    first commit

---
+++ app/[locale]/(default)/layout.tsx
@@ -0,0 +1,23 @@
+import Footer from "@/components/blocks/footer";
+import Header from "@/components/blocks/header";
+import { ReactNode } from "react";
+import { getLandingPage } from "@/services/page";
+
+export default async function DefaultLayout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  const page = await getLandingPage(locale);
+
+  return (
+    <>
+      {page.header && <Header header={page.header} />}
+      <main className="overflow-x-hidden">{children}</main>
+      {page.footer && <Footer footer={page.footer} />}
+    </>
+  );
+}
